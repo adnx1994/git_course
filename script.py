98f08237753c700fee47caad4c7a39a40c6d2660
Alireza <169320894+adnx1994@users.noreply.github.com>
2024-11-26 01:25:37 +0330

    fetching data func

--- script.py
+++ script.py
@@ -6,9 +6,31 @@ import finpy_tse as fpy
 import pytse_client as tse
 import yfinance as yf
 import ta 
+import asyncio
+
+
+symbol_analyze="BTC-USD"
 
 
 last_day =datetime.now()
 last_day=last_day.date()
 last_day=str(last_day)
 last_day
+
+
+
+
+
+def yahoo_symbol(symbol="BTC-USD",timeframe='1d'):
+    while True:
+        try:
+            data = yf.download(symbol,period='max',interval= timeframe)
+            return(data)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            asyncio.sleep(2)  # Sleep for 10 seconds before retrying
+
+
+
+symbol_data=yahoo_symbol(symbol=symbol_analyze)
+symbol_data
