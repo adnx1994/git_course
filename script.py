3808e9b95817d198346caadbae16c3fcca277773
Alireza <169320894+adnx1994@users.noreply.github.com>
2024-11-26 01:23:36 +0330

    import modules

---
+++ script.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import finpy_tse as fpy
+import pytse_client as tse
+import yfinance as yf
+import ta 
